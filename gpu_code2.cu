90c9f950d6cd5a426faa8954a5328fb2664e606b
NipamB <nipambasumatary1@gmail.com>
2020-07-22 00:09:05 +0530

    corrected size of rand_int_fruitx/y

--- gpu_code2.cu
+++ gpu_code2.cu
@@ -20,6 +20,10 @@ using namespace std;
 #define natural_selection_rate 0.2
 #define mutation_rate 0.01
 #define generations 300
+#define negative_reward -150
+#define positive_reward 500
+#define max_total_steps 500
+#define threshold_view_game 50
 
 ////////////////////////////////////////////////
 //for color
@@ -321,6 +325,9 @@ __global__ void play_game(float *nns, float *fitness, unsigned int *random_int_f
     fruitx = random_int_fruitx[fruit_index] % width;
     fruity = random_int_fruity[fruit_index] % height;
 
+    // if(id == 0)
+    //     printf("%d:%d ",fruitx,fruity);
+
     fruit_index++;
 
     //snake length
@@ -343,9 +350,6 @@ __global__ void play_game(float *nns, float *fitness, unsigned int *random_int_f
     __shared__ float input[ni];
     __shared__ float hidden_output[nh];
     __shared__ float output[no];
-
-    int reward_value;
-    reward_value = 500;
 
     __shared__ int break_flag;
     break_flag = 0;
@@ -484,7 +488,7 @@ __global__ void play_game(float *nns, float *fitness, unsigned int *random_int_f
 
             if(x >= width || x < 0 || y >= height || y < 0)
             {
-                reward = -150;
+                reward = negative_reward;
                 break_flag = 1;
             }
 
@@ -492,7 +496,7 @@ __global__ void play_game(float *nns, float *fitness, unsigned int *random_int_f
             {
                 if(tailx[i]==x && taily[i]==y)
                 {
-                    reward = -150;
+                    reward = negative_reward;
                     break_flag = 1;
                 }
             }
@@ -503,8 +507,7 @@ __global__ void play_game(float *nns, float *fitness, unsigned int *random_int_f
                 fruity = random_int_fruity[fruit_index] % height;
                 fruit_index++;
                 ntail++;
-                reward = reward_value;
-                // reward_value *= 2;
+                reward = positive_reward;
             }
             
             total_reward += reward;
@@ -519,7 +522,7 @@ __global__ void play_game(float *nns, float *fitness, unsigned int *random_int_f
             reward = 0;
 
             if(reward > 0)
-                total_steps = (total_steps+100 > 500) ? 500 : total_steps + 100;
+                total_steps = (total_steps+100 > max_total_steps) ? max_total_steps : total_steps + 100;
 
             if(steps > total_steps){
                 break_flag = 1;
@@ -1090,7 +1093,7 @@ void logic(){
 
     if(x==fruitx && y==fruity)
     {
-        score = score +1000;
+        score = score + 1;
         fruitx=rand() % width;
         fruity=rand() % height;
         ntail++;
@@ -1197,8 +1200,8 @@ int main(){
         // curandSetStream(prng,stream1);
 
         // create random number generator for integer values of fruit
-        curandGenerate(prng,random_int_fruitx,parameter_size);
-        curandGenerate(prng,random_int_fruity,parameter_size);
+        curandGenerate(prng,random_int_fruitx,population_size*max_snake_length);
+        curandGenerate(prng,random_int_fruity,population_size*max_snake_length);
         
         play_game<<<population_size,parameter_size,parameter_size*sizeof(float)>>>(dnns,dfitness,random_int_fruitx,random_int_fruity,parameter_size);
         
@@ -1231,8 +1234,10 @@ int main(){
 
         // cudaDeviceSynchronize();
 
-        if(k > 50)
-        view_game(best_snake);
+        if(k > threshold_view_game)
+            view_game(best_snake);
+
+        // printf("Score: %d\n",score);
         
         int top = population_size * natural_selection_rate;
 
